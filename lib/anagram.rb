d358995efe327d3f9a8c793c52b1a53a3327b15c
tgray017 <tgray017@gmail.com>
2018-10-18 01:07:07 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,8 +8,8 @@ class Anagram
   def match(word_array)
     anagrams = []
     word_array.each do |word|
-      letters_array = word.split("").sort.join
-      anagrams << 
+      sorted_word = word.split("").sort.join
+      anagrams << word if sorted_word == @word
     end
     anagrams
   end
